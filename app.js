b240c2f239197fe256e9827450d206e56922cecb
Erik5CA <erik.castillo.anzures@gmail.com>
2024-07-04 15:50:09 -0600

    test #skip

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 // get the port from env variable
 const PORT = process.env.PORT || 5000;
 // coment to test new version
+// coment to test skip tag
 app.use(express.static("dist"));
 // Health Check Endpoint
 app.get("/health", (req, res) => {
